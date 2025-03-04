d71d124284b7249bd2732c4c6be7829245824ac8
Shaun <ongshaun0902@gmail.com>
2025-03-04 23:31:32 +0800

    Converted CSS to Tailwind

--- script.js
+++ script.js
@@ -6,35 +6,43 @@ fetch('data.json')
 
         // Tabs Header Group 
         const tabHeader = document.createElement('div');
-        tabHeader.className = 'tab-header';
+        tabHeader.className = 'flex border-b border-slate-500';
+        tabHeader.id = 'tabHeader';
 
         // Tabs Content Box
         const tabContent = document.createElement('div');
-        tabContent.className = 'tab-content';
+        tabContent.className = 'p-5 border border-slate-500 border-t-0 bg-slate-200 hidden';
+        tabContent.id = 'tabContent';
 
         data.forEach((section, index) => {
 
             // Tab Button Element Creation
             const tabButton = document.createElement('button');
+            tabButton.className = 'flex-1 p-2.5 bg-slate-300 cursor-pointer transition-colors duration-300 rounded-t-lg text-x1 font-bold';
+            tabButton.id = 'tabButton';
             tabButton.textContent = section.title;
 
             // Accordion Item Element Creation
             const accordionItem = document.createElement('div');
-            accordionItem.className = 'accordion-item';
+            accordionItem.className = 'border-b border-slate-500';
+            tabButton.id = 'tabButton';
 
             // Accordion Header Element Creation
             const accordionHeader = document.createElement('div');
-            accordionHeader.className = 'accordion-header';
+            accordionHeader.className = 'flex-1 p-2.5 bg-slate-300 cursor-pointer transition-colors duration-300 rounded-t-lg text-x1 font-bold hover:bg-slate-400';
             accordionHeader.textContent = section.title;
+            accordionHeader.id = 'accordionHeader';
 
             // Accordion Content Element Creation
             const accordionContent = document.createElement('div');
-            accordionContent.className = 'accordion-content';
+            accordionContent.className = 'p-2.5 bg-slate-200 hidden';
             accordionContent.innerHTML = section.content;
+            accordionContent.id = 'accordionContent';
 
             // Arrow Icon Element Creation
             const toggleIcon = document.createElement('span');
-            toggleIcon.className = 'toggle-icon';
+            toggleIcon.className = 'w-5 h-5 bg-contain bg-center bg-no-repeat';
+            toggleIcon.id = 'toggleIcon';
             accordionHeader.appendChild(toggleIcon);
 
 
@@ -43,24 +51,28 @@ fetch('data.json')
 
                 element.addEventListener('click', () => {
                     // Close accordion if it is opened
-                    if(accordionHeader.classList.contains('active')){
-                        accordionHeader.classList.remove('active');
-                        accordionContent.style.display = 'none';
+                    if(accordionHeader.classList.contains('bg-slate-500')){
+                        //Select to Unselect
+                        accordionHeader.classList.replace('bg-slate-500', 'bg-slate-300');
+                        //toggleIcon remove up arrow
+                        //toggleIcon add down arrow
+                        accordionContent.classList.add('hidden');
                     } else{
                         // Selecting a tab
-                        document.querySelectorAll('.tab-header button').forEach(btn => btn.classList.remove('active'));
-                        document.querySelectorAll('.tab-content').forEach(content => content.style.display = 'none');
+                        document.querySelectorAll('#tabButton').forEach(btn => btn.classList.replace('bg-slate-500', 'bg-slate-300'));
+                        document.querySelectorAll('#tabContent').forEach(content => content.classList.replace('block', 'hidden'));
     
-                        tabButton.classList.add('active');
+                        tabButton.classList.replace('bg-slate-300', 'bg-slate-500');
                         tabContent.innerHTML = section.content;
-                        tabContent.style.display = 'block';
+                        tabContent.classList.replace('hidden', 'block');
 
                         // Selecting an accordion
-                        document.querySelectorAll('.accordion-header').forEach(header => header.classList.remove('active'));
-                        document.querySelectorAll('.accordion-content').forEach(content => content.style.display = 'none');
+                        document.querySelectorAll('#accordionHeader').forEach(header => header.classList.replace('bg-slate-500', 'bg-slate-300') /*Add replacing arrow heads*/);
+                        document.querySelectorAll('#accordionContent').forEach(content => content.classList.replace('block', 'hidden'));
     
-                        accordionHeader.classList.add('active');
-                        accordionContent.style.display = 'block';
+                        accordionHeader.classList.replace('bg-slate-300', 'bg-slate-500');
+                        accordionContent.classList.replace('hidden', 'block');
+                        //Add arrow head toggling
 
                     }
                 });
@@ -69,12 +81,12 @@ fetch('data.json')
 
             // Open first section when first launched
             if (index === 0){
-                tabButton.classList.add('active');
+                tabButton.classList.replace('bg-slate-300', 'bg-slate-500');
                 tabContent.innerHTML = section.content;
-                tabContent.style.display = 'block';
+                tabContent.classList.remove('hidden');
 
-                accordionHeader.classList.add('active');
-                accordionContent.style.display = 'block';
+                accordionHeader.classList.replace('bg-slate-300', 'bg-slate-500');
+                accordionContent.classList.remove('hidden');
             }
 
             tabHeader.appendChild(tabButton);
